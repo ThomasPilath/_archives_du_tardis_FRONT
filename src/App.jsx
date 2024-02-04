41caf2b0c7718336a6c9117addfe868ea9d5a566
Thomas_Pi <pijat.thomas@gmail.com>
2024-02-04 10:44:06 +0100

    ADD First Look component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import TestElement from './pages/TestElement'
 import Home from './pages/Home'
+import FirstLook from './components/FirstLook/FirstLook'
 
 export default function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<FirstLook />} />
+				<Route path="/home" element={<Home />} />
 				<Route path="/test" element={<TestElement />} />
 			</Routes>
 		</>
---
+++ src/components/FirstLook/FirstLook.jsx
@@ -0,0 +1,83 @@
+import './FirstLook.css'
+export default function FirstLook() {
+  return (
+    <>
+      <aside class="arrival">
+        <div class="tHold">
+          <div class="tPos">
+              <div class="tRot">
+                  <div class="tSide tSideF">
+                      <div class="tSign"></div>
+                      <div class="tTop"></div>
+                      <div class="tBaseSide"></div>
+                      <div class="tPanel tPanelL">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                      <div class="tPanel tPanelR">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                          <div class="tNotice"></div>
+                      </div>
+                  </div>
+                  <div class="tSide tSideL">
+                      <div class="tSign"></div>
+                      <div class="tTop"></div>
+                      <div class="tBaseSide"></div>
+                      <div class="tPanel tPanelL">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                      <div class="tPanel tPanelR">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                  </div>
+                  <div class="tSide tSideR">
+                      <div class="tSign"></div>
+                      <div class="tTop"></div>
+                      <div class="tBaseSide"></div>
+                      <div class="tPanel tPanelL">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                      <div class="tPanel tPanelR">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                  </div>
+                  <div class="tSide tSideB">
+                      <div class="tSign"></div>
+                      <div class="tTop"></div>
+                      <div class="tBaseSide"></div>
+                      <div class="tPanel tPanelL">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                      <div class="tPanel tPanelR">
+                          <div class="tWindowT"></div>
+                          <div class="tWindowB"></div>
+                      </div>
+                  </div>
+                  <div class="tSignTop"></div>
+                  <div class="tSignTop tBase"></div>
+                  <div class="tLight">
+                      <div class="tLightSide"></div>
+                      <div class="tLightSide tLightSide1"></div>
+                      <div class="tLightSide tLightSide2"></div>
+                      <div class="tLightSide tLightSide3"></div>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <div class="vortex">
+            <div class="vSpark"></div>
+            <div class="vCloud"></div>
+        </div>
+      </aside>
+      <section class="button">
+          <a href="/home"><button>Entrer dans l'Univers</button></a>
+      </section>
+    </>
+  )
+}
