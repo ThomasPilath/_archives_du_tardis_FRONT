866afda2eda654c2c7d5b7711c1919d38be64e31
Thomas_Pi <pijat.thomas@gmail.com>
2024-02-08 18:47:40 +0100

    update .env

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -4,9 +4,10 @@ export const useApi = () => {
     "Content-Type": "application/json",
     "Access-control-Allow-Origin": "*"
   };
-  // const apiUrl = import.meta.env.VITE_API_URL
+  const apiUrl = import.meta.env.VITE_API_URL
   const api = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
+    baseURL: apiUrl,
+    // baseURL: "https://jsonplaceholder.typicode.com",
     headers
   });
 
