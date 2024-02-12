56585ce0fb9773b8c8b4a16bb517596b1adf3076
Thomas_Pi <pijat.thomas@gmail.com>
2024-02-12 20:10:43 +0100

    TEST .env title

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,11 +3,12 @@ import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 
 export default function AppLayout(props) {
+  const title = import.meta.env.VITE_SITE_TITLE
   return (
     <>
       <h1 className="w-full mx-auto font-title text-8xl text-white pt-6">
         <Link to={"/"}>
-          Les Archives du Tardis
+          {title}
         </Link>
       </h1>
       <NavBar />
