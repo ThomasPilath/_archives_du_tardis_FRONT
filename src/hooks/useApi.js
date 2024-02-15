3056a293bd957172816f40b1069b5f6e043c8e15
Thomas_Pi <pijat.thomas@gmail.com>
2024-02-15 16:31:54 +0100

    TEST encore

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -6,12 +6,8 @@ export const useApi = () => {
     "Access-control-Allow-Origin": "*"
   };
 
-  console.log("VITE :",import.meta.env.VITE_BASE_URL)
-  console.log("BASE :",import.meta.env.BASE_URL)
-  const apiUrl = import.meta.env.VITE_BASE_URL || import.meta.env.BASE_URL
-
   const api = axios.create({
-    baseURL: apiUrl,
+    baseURL: import.meta.env.VITE_BASE_URL,
     headers
   });
 
