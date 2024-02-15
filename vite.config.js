b703f32c6c9bda4329639a57629e341f9abaa08e
Thomas_Pi <pijat.thomas@gmail.com>
2024-02-15 22:27:54 +0100

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  define: {
-    'process.env': JSON.stringify(process.env)
-  },
 })
